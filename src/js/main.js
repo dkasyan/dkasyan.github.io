f85a2ae649636295607690d0bf4499dff17b856d
dkasyan <damian.kasyan@hotmail.com>
2019-11-25 19:56:15 +0100

    Add: JS project api

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,34 @@
 "use strict";
 
-// service worker registration - remove if you're not going to use it
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
 
 // place your code below
 
-fetch("https://api.github.com/users/dkasyan/repos")
+fetch('https://api.github.com/users/dkasyan/repos?sort=updated&direction=desc')
   .then(resp => resp.json())
   .then(resp => {
-    console.log(resp)
+    const repos = resp;
+    for (const repo of repos){
+      const {description, homepage, html_url, name} = repo;
+      console.log(repo.name)
+      console.log(repo.html_url)
+      list.innerHTML += `
+      <li class="projects_list_item">
+      <div class="projects_list_item_Container">
+        <img src="assets/img/GithubIcon.svg">
+        <h3 class="project_list_title">${name}</h3>
+        ${
+          description ? `<p class="project_list_description">${description}</p>` : 'No description'
+        }        
+      </div>
+      <div class="projects_list_footer">
+          <a class="projects_list_footer_link" href="#" title="Demo:Pierwsze">Demo</a>
+          <a class="projects_list_footer_link" href="#" title="Source code: Pierwszerepo">Github</a>
+      </div>
+      </li>
+      `;
+
+    }
   })
 
 console.log(`Hello world!`);
